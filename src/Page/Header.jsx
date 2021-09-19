eea7598b10eb9754cf200af4505419853a8e0203
Jarquise Turner <j.turner348@gmail.com>
2021-09-19 18:53:33 -0400

    Create Header.jsx

---
+++ src/Page/Header.jsx
@@ -0,0 +1,25 @@
+
+import { Link } from 'react-router-dom'
+
+import React from 'react'
+
+function NavBar() {
+    return (
+        <nav id="nav-bar">
+            <ul>
+                <li>
+                    <Link to="/Home"> Home </Link>
+                </li>
+               
+                <li>
+                    <Link to="/Coin"> Crypto Price </Link>
+                </li>
+                <li>
+                    <Link to="/contact"> Contact </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
